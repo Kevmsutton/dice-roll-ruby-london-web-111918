314af6b041f0efc1d33e13fbc53303ad08dd9a02
Kevin Sutton <kevmsutton@gmail.com>
2018-11-05 11:35:41 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,17 +2,6 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-
-
-
-def roll
-  rand(1..6)
-end
-
-def roll
-  1 + rand(6)
-end
-
 def roll
   roll = [1,2,3,4,5,6]
   roll.sample
